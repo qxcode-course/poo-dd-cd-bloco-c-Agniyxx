091f48c610b358a2fbfba19dd61a6956393092cf
Agne <agne.steph.moss@gmail.com>
2025-11-01 19:09:41 -0300

    configurando o novo repo e començando as nova atvds

---
+++ myrep/poo/budega/py/draft.py
@@ -0,0 +1,3 @@
+class Cliente:
+    def __init__(self, nome: str):
+        self.__nome 
--- setup_ts.sh
+++ setup_ts.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+  #!/usr/bin/env bash
 
 # Instala pacotes Node.js globais
 npm install -g typescript esbuild
