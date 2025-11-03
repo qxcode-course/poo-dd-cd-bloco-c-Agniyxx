368d7e247ca00cd70b7f76778684b3c32754e4a9
Agne <agne.steph.moss@gmail.com>
2025-11-03 16:45:52 -0300

    continuando atvd budega

--- myrep/poo/budega/py/draft.py
+++ myrep/poo/budega/py/draft.py
@@ -9,16 +9,25 @@ class Person:
         return f"{self.__name}"
 
 class Market:
-    def __init__(self, counters: list, waiting: str):
-        self.counters = [None] 
-        self.waiting = waiting
+    def __init__(self, num_counters: int):
+        self.counters: Person | None = [] 
+        for _ in range(num_counters):
+            self.counters = None
+        self.waiting: Person = []
     
     def __str__(self):
         return f"Caixas: [{self.counters}]\Espera: [{self.waiting}]"
     
+    def arrive(slef, person: Person):
+
+
+    def call(self, index: int):
+
+    def finish(index:int): Person | None
+    
 
 def main():
-    mercado = Market(0, "")
+    mercado = Market()
     while True:
         line = input()
         print(f"${line}")
@@ -34,3 +43,7 @@ def main():
             mercado = Market(counters, waiting)
             
 main()
+
+#.append() cria  a lista 
+#join contrariio do split 
+#
